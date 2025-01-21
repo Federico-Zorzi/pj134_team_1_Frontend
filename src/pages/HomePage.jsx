027d83688d113af0fbfafdc9eb130e6a6320de7b
FedericoBellezza <federico.bellezza2001@gmail.com>
2025-01-21 17:41:55 +0100

    progress

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,8 +3,9 @@ import { Link } from "react-router-dom";
 
 export default function HomePage() {
   //* take data from global context
-  const { properties } = useDataContext();
-  console.log(properties);
+  const { tempDataProperties } = useDataContext();
+
+  console.log(tempDataProperties);
 
   return (
     <main>
@@ -12,37 +13,30 @@ export default function HomePage() {
         <h1 className="my-5">I più gettonati✨</h1>
         <div className="row g-3 homepage-card-container">
           {/* cards */}
-          {properties.map((immobile) => {
+          {tempDataProperties.map((immobile) => {
             return (
-              <>
-                <Link
-                  className="col-3 position-relative"
-                  to={`/post/${immobile.id}`}
-                  key={immobile.id}
-                >
-                  <div className="card">
-                    <img
-                      src={
-                        "https://hips.hearstapps.com/hmg-prod/images/torino-with-mole-antonelliana-and-the-alps-royalty-free-image-1643015862.jpg"
-                      }
-                      className="card-img-top"
-                    />
-                    <div className="card-body">
-                      <h5 className="card-title fs-5">{immobile.title}</h5>
-                      <p className="card-text">Host: {immobile.host}</p>
+              <div className="col-3 position-relative" key={immobile.id}>
+                <Link className="card">
+                  <img
+                    src={
+                      "https://hips.hearstapps.com/hmg-prod/images/torino-with-mole-antonelliana-and-the-alps-royalty-free-image-1643015862.jpg"
+                    }
+                    className="card-img-top"
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title fs-5">{immobile.title}</h5>
+                    <p className="card-text">Host: Tizio Caio</p>
+                    <div className="row">
+                      <span className="card-text col-6">
+                        €{immobile.price}/notte
+                      </span>
+                      <span className="card-text likes-card-text col-6 text-align-end">
+                        <span>{immobile.property_type}</span>
+                      </span>
                     </div>
                   </div>
                 </Link>
-                <div className="row position-absolute">
-                  <span className="card-text col-6">
-                    €{immobile.price}/notte
-                  </span>
-                  <span className="card-text likes-card-text col-6 text-align-end">
-                    <span onClick={() => console.log(immobile.likes)}>❤</span>
-                    {immobile.likes}
-                  </span>
-                </div>
-              </>
+              </div>
             );
           })}
         </div>
