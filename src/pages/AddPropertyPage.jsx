7c03cfa8a7a9c134b1b32250e63f32b5a0dd58d5
Federico-Zorzi <federico.zorzi05@gmail.com>
2025-01-22 09:36:02 +0100

    components removed

--- src/pages/AddPropertyPage.jsx
+++ src/pages/AddPropertyPage.jsx
@@ -1,5 +1,3 @@
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
 import AddPropertyForm from "../components/AddPropertyForm";
 
 export default function AddPropertyPage() {
