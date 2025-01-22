ea830a20d407430ea45a63059535f395bc6849de
Federico-Zorzi <federico.zorzi05@gmail.com>
2025-01-22 18:13:32 +0100

    stars

--- src/components/reviews/ReviewItem.jsx
+++ src/components/reviews/ReviewItem.jsx
@@ -1,6 +1,28 @@
 import { Card, Col, Row } from "react-bootstrap";
 
 export default function ReviewItem({ review }) {
+  // FUNCTION FOR RATE WITH STARS
+  const rateStarsConversion = (rate) => {
+    let decodStars = [];
+    const numbTrunc = Math.trunc(rate);
+    const numOfStars = Math.round(rate);
+
+    for (let i = 1; i <= 5; i++) {
+      if (i <= numOfStars) {
+        if (i == numOfStars && rate / 2 - numbTrunc >= 0.5) {
+          decodStars.push(
+            <i className="star fa-solid fa-star-half-stroke"></i>
+          );
+        } else {
+          decodStars.push(<i className="star fa-solid fa-star"></i>);
+        }
+      } else {
+        decodStars.push(<i className="star fa-regular fa-star"></i>);
+      }
+    }
+    return decodStars;
+  };
+
   return (
     <Col xs={12} md={6} className="g-3">
       <Card className="card cards-review">
@@ -10,7 +32,9 @@ export default function ReviewItem({ review }) {
               <span className="review-user-photo">P</span>
               {review.name}
             </Col>
-            <Col className="vote text-end">{review.vote}</Col>
+            <Col className="vote text-end">
+              {rateStarsConversion(review.vote)}
+            </Col>
           </Row>
         </Card.Header>
         <Card.Body>
