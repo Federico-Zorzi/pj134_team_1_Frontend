a549ae4044798e667b738fbc1e6063b8bb488e51
FedericoBellezza <federico.bellezza2001@gmail.com>
2025-01-22 14:32:25 +0100

    progress

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,9 +3,10 @@ import { Link } from "react-router-dom";
 
 import { useEffect, useState } from "react";
 import HomepageCard from "../components/HomepageCard";
+import { Button } from "react-bootstrap";
 
 export default function HomePage() {
-  //* take data from global context
+  // take data from global context
   const dataContext = useDataContext();
 
   const { propertiesList, fetchIndexProperties } = dataContext;
@@ -14,7 +15,12 @@ export default function HomePage() {
   return (
     <main>
       <div className="container py-3">
-        <h1 className="my-5">I più gettonati✨</h1>
+        <div className="d-flex justify-content-between align-items-center my-5">
+          <h1>I più gettonati✨</h1>
+          <Button as={Link} to="/advanceSearch">
+            Vai alla ricerca avanzata
+          </Button>
+        </div>
 
         <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-3 homepage-card-container">
           {/* Cards */}
