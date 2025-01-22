b00b4dc14e62325b1f2d5285ec3ead0deef1b859
FedericoBellezza <federico.bellezza2001@gmail.com>
2025-01-22 13:01:05 +0100

    progress

--- src/pages/AdvanceSearchPage.jsx
+++ src/pages/AdvanceSearchPage.jsx
@@ -3,18 +3,34 @@ import { useDataContext } from "../context/dataContext";
 import { useEffect, useState } from "react";
 import HomepageCard from "../components/HomepageCard";
 import SearchBar from "../components/SearchBar";
+import { Badge } from "react-bootstrap";
 
 export default function AdvanceSearchPage() {
+  const badges = ["Filtro 1", "Filtro 2", "Filtro 3"];
+
   // take data from global context
   const dataContext = useDataContext();
-
   const { propertiesList, fetchIndexProperties } = dataContext;
   useEffect(fetchIndexProperties, []);
+
+  //   Toggle badge function
+  const toggleBadge = () => {
+    console.log();
+  };
+
   return (
     <>
       <div className="container py-5 min-vh-100">
         <h1>Advance Search</h1>
-
+        <span>
+          {badges.map((badge) => {
+            return (
+              <Badge className="me-1" key={badge}>
+                {badge}
+              </Badge>
+            );
+          })}
+        </span>
         <SearchBar />
         <div className="my-5"></div>
         {/* card container */}
