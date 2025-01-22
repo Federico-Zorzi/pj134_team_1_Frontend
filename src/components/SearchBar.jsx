aa4c7e6e9e36c448a1db48d8d68a35bcc29fbd38
MattiaPlacido <placido.mattia@gmail.com>
2025-01-22 11:57:29 +0100

    Testing page aggiunta

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,5 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 import * as bootstrap from "bootstrap";
 import { useState } from "react";
 
@@ -21,6 +22,7 @@ export default function SearchBar() {
       ...prevData,
       [name]: newValue,
     }));
+    console.log(formData);
   };
 
   const handleSearchSubmit = (e) => {
@@ -40,6 +42,39 @@ export default function SearchBar() {
             placeholder="Cerca il tuo immobile"
           />
         </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label>Numero di stanze</Form.Label>
+          <Form.Control
+            type="number"
+            value={formData.nRooms}
+            name="nRooms"
+            onChange={handleInputChange}
+            placeholder="Numero di stanze"
+            min="1"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label>Numero di letti</Form.Label>
+          <Form.Control
+            type="number"
+            value={formData.nBeds}
+            name="nBeds"
+            onChange={handleInputChange}
+            placeholder="Numero di stanze"
+            min="1"
+          />
+        </Form.Group>
+        <Form.Select
+          aria-label="Default select example"
+          value={formData.propertyType}
+          name="propertyType"
+          onChange={handleInputChange}
+        >
+          <option>Seleziona il tipo di proprietà</option>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+        </Form.Select>
         <Button variant="primary" type="submit">
           Cerca
         </Button>
---
+++ src/pages/TestingPage.jsx
@@ -0,0 +1,8 @@
+import SearchBar from "../components/SearchBar";
+export default function TestingPage() {
+  return (
+    <div className="bg-dark text-white">
+      <SearchBar />
+    </div>
+  );
+}
