b27a5ebecd012c34a942bd69006ff9157e7be5c1
FedericoBellezza <federico.bellezza2001@gmail.com>
2025-01-22 12:05:25 +0100

    progress

---
+++ src/components/HomepageCard.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+export default function HomepageCard(params) {
+  const property = params.element;
+
+  //   function to add like
+  const [like, setLike] = useState(property.likes);
+  const addLike = (id) => {
+    const url = `http://localhost:3000/properties/${id}/addlike`;
+    setLike(like + 1);
+    fetch(url, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
+
+  return (
+    <div className="col-3 position-relative" key={property.id}>
+      <Link className="card" to={`/${property.id}`}>
+        <img
+          onClick={() => console.log(property)}
+          src={
+            "https://hips.hearstapps.com/hmg-prod/images/torino-with-mole-antonelliana-and-the-alps-royalty-free-image-1643015862.jpg"
+          }
+          className="card-img-top"
+        />
+        <div className="card-body">
+          <h5 className="card-title fs-5">{property.title}</h5>
+          <p className="card-text">
+            <i>{property.city}</i>
+          </p>
+          <div className="row">
+            <span className="card-text col-6">€120/notte</span>
+            <span className="card-text property-type-card-text col-6 text-align-end">
+              <span>{property.property_type}</span>
+            </span>
+          </div>
+        </div>
+      </Link>
+      <a
+        className="likes-card-text"
+        onClick={() => {
+          addLike(property.id);
+        }}
+      >
+        ❤{like}
+      </a>
+    </div>
+  );
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,8 @@
 import { useDataContext } from "../context/dataContext";
 import { Link } from "react-router-dom";
+
 import { useEffect, useState } from "react";
+import HomepageCard from "../components/HomepageCard";
 
 export default function HomePage() {
   //* take data from global context
@@ -9,53 +11,15 @@ export default function HomePage() {
   const { propertiesList, fetchIndexProperties } = dataContext;
   useEffect(fetchIndexProperties, []);
 
-  // funzione per aggiungere i likes
-  const addLike = (id) => {
-    const property = propertiesList.find((property) => property.id === id);
-    property.likes++;
-    console.log(property.likes);
-  };
-
   return (
     <main>
       <div className="container pt-3">
         <h1 className="my-5">I più gettonati✨</h1>
+
         <div className="row g-3 homepage-card-container">
           {/* cards */}
           {propertiesList.map((property) => {
-            return (
-              <div className="col-3 position-relative" key={property.id}>
-                <Link className="card">
-                  <img
-                    onClick={() => console.log(property)}
-                    src={
-                      "https://hips.hearstapps.com/hmg-prod/images/torino-with-mole-antonelliana-and-the-alps-royalty-free-image-1643015862.jpg"
-                    }
-                    className="card-img-top"
-                  />
-                  <div className="card-body">
-                    <h5 className="card-title fs-5">{property.title}</h5>
-                    <p className="card-text">
-                      <i>{property.city}</i>
-                    </p>
-                    <div className="row">
-                      <span className="card-text col-6">€120/notte</span>
-                      <span className="card-text property-type-card-text col-6 text-align-end">
-                        <span>{property.property_type}</span>
-                      </span>
-                    </div>
-                  </div>
-                </Link>
-                <a
-                  className="likes-card-text"
-                  onClick={() => {
-                    addLike(property.id);
-                  }}
-                >
-                  ❤{property.likes}
-                </a>
-              </div>
-            );
+            return <HomepageCard key={property.id} element={property} />;
           })}
         </div>
       </div>
