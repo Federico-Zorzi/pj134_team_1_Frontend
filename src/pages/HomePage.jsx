e29838ef7f368ce1185eb06401848eb964aab6e8
Federico-Zorzi <federico.zorzi05@gmail.com>
2025-01-23 13:34:41 +0100

    card homepage modified

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -27,7 +27,7 @@ export default function HomePage() {
           </Button>
         </div>
 
-        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-3 homepage-card-container">
+        <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 g-3 homepage-card-container">
           {/* Cards */}
           {propertiesList.map((property) => {
             return (
