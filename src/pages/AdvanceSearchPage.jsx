903ec4fda1970de04774064e4e9c09a5798df0cb
FrancescoGiommi <francescogiommi97@icloud.com>
2025-01-23 16:07:58 +0100

    Add fix class

--- src/pages/AdvanceSearchPage.jsx
+++ src/pages/AdvanceSearchPage.jsx
@@ -17,7 +17,7 @@ export default function AdvanceSearchPage() {
         <SearchBar propertiesList={propertiesList} />
         <div className="my-5"></div>
         {/* card container */}
-        <div class="row row-cols-1 row-cols-sm-1 row-cols-md-1 row-cols-lg-2 g-4 homepage-card-container">
+        <div className="row row-cols-1 row-cols-sm-1 row-cols-md-1 row-cols-lg-2 g-4 homepage-card-container">
           {/* Cards */}
           {propertiesList.map((property) => {
             return <AdvancedSearchCard key={property.id} element={property} />;
