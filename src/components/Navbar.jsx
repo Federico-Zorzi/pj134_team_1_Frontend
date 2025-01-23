2be3d075f1cb85c0ce2efd5ef833e72e25918720
MattiaPlacido <placido.mattia@gmail.com>
2025-01-23 10:46:28 +0100

    Login temporaneo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,15 @@
 import { NavLink } from "react-router-dom";
+import { useDataContext } from "../context/dataContext";
 
 export default function Navbar() {
+  const { userData } = useDataContext();
+  const { isUserOwner, temporaryLogin } = userData;
+
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
-        <NavLink className="navbar-brand" to="/">
-          Posts
+        <NavLink className="navbar-brand fw-bold fs-2" to="/">
+          BoolB&B
         </NavLink>
         <button
           className="navbar-toggler"
@@ -19,19 +23,29 @@ export default function Navbar() {
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
+          <ul className="navbar-nav mx-auto">
             <li className="nav-item">
               <NavLink className="nav-link active" aria-current="page" to="/">
                 Home
               </NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link" to="/aboutUs">
-                About Us
+              <NavLink className="nav-link" to="/store">
+                Aggiungi un immobile!
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className="nav-link" to="/advanceSearch">
+                Cerca
               </NavLink>
             </li>
           </ul>
         </div>
+      </div>
+      <div className="mx-3 fs-5" role="button">
+        <p onClick={() => temporaryLogin()}>
+          {isUserOwner ? "Logout" : "Login"}
+        </p>
       </div>
     </nav>
   );
