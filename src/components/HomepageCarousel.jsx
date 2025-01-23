7d084616476ae9b8e2dbe79bb845e654766dccb9
FedericoBellezza <federico.bellezza2001@gmail.com>
2025-01-23 20:31:42 +0100

    fix carousel

--- src/components/HomepageCarousel.jsx
+++ src/components/HomepageCarousel.jsx
@@ -8,33 +8,30 @@ export default function UncontrolledExample() {
           src="https://dev-snowit.ams3.digitaloceanspaces.com/uploads/2019/03/cene-in-baita.jpg"
           text="First slide"
         />
-        <Carousel.Caption>
+        <div className="carousel-caption2">
           <h3>Chalet nelle Alpi</h3>
-          <p>Nulla di meglio per rilassarsi tra la neve.</p>
-        </Carousel.Caption>
+          <span>Nulla di meglio per rilassarsi tra la neve.</span>
+        </div>
       </Carousel.Item>
       <Carousel.Item>
         <img
           src="https://www.vrbo.com/it-it/idee-vacanze/wp-content/uploads/5UTesrQHCfZDv7RCI3M4ei/a10dc7d17671ec2cf249dd6add39c70c/hero.jpg"
           text="Second slide"
         />
-        <Carousel.Caption>
+        <div className="carousel-caption2">
           <h3>Vista mare</h3>
-          <p>Vuoi scappare da queste fredde giornate?</p>
-        </Carousel.Caption>
+          <span>Vuoi scappare da queste fredde giornate?</span>
+        </div>
       </Carousel.Item>
       <Carousel.Item>
         <img
           src="https://asset.montecarlosbm.com/styles/hero_image_desktop_wide_webp/s3/media/image/villa-la-vue-los-angeles-10-plus-belles-maisons-monde.jpg.webp?h=97cb7b08&itok=i1DY7ana"
           text="Third slide"
         />
-        <Carousel.Caption>
+        <div className="carousel-caption2">
           <h3>Notte di lusso</h3>
-          <p>
-            Concediti la possibilità di vivere una notte di lusso in una villa
-            mozzafiato.
-          </p>
-        </Carousel.Caption>
+          <span>Concediti la possibilità di vivere una notte mozzafiato.</span>
+        </div>
       </Carousel.Item>
     </Carousel>
   );
