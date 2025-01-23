17fd40ddef44e986f44a5cfb2ac1fd856eaaf25e
FedericoBellezza <federico.bellezza2001@gmail.com>
2025-01-23 20:07:00 +0100

    progress

--- src/components/HomepageCarousel.jsx
+++ src/components/HomepageCarousel.jsx
@@ -1,29 +1,38 @@
 import Carousel from "react-bootstrap/Carousel";
-import ExampleCarouselImage from "components/ExampleCarouselImage";
 
 export default function UncontrolledExample() {
   return (
-    <Carousel>
+    <Carousel className="homepage-carousel mb-5">
       <Carousel.Item>
-        <ExampleCarouselImage text="First slide" />
+        <img
+          src="https://dev-snowit.ams3.digitaloceanspaces.com/uploads/2019/03/cene-in-baita.jpg"
+          text="First slide"
+        />
         <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          <h3>Chalet nelle Alpi</h3>
+          <p>Nulla di meglio per rilassarsi tra la neve.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <ExampleCarouselImage text="Second slide" />
+        <img
+          src="https://www.vrbo.com/it-it/idee-vacanze/wp-content/uploads/5UTesrQHCfZDv7RCI3M4ei/a10dc7d17671ec2cf249dd6add39c70c/hero.jpg"
+          text="Second slide"
+        />
         <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <h3>Vista mare</h3>
+          <p>Vuoi scappare da queste fredde giornate?</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <ExampleCarouselImage text="Third slide" />
+        <img
+          src="https://asset.montecarlosbm.com/styles/hero_image_desktop_wide_webp/s3/media/image/villa-la-vue-los-angeles-10-plus-belles-maisons-monde.jpg.webp?h=97cb7b08&itok=i1DY7ana"
+          text="Third slide"
+        />
         <Carousel.Caption>
-          <h3>Third slide label</h3>
+          <h3>Notte di lusso</h3>
           <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            Concediti la possibilità di vivere una notte di lusso in una villa
+            mozzafiato.
           </p>
         </Carousel.Caption>
       </Carousel.Item>
