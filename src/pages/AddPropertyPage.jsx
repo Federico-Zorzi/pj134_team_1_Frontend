d4e6ccfb08794171d92e1409e0633741ab52cb2e
MattiaPlacido <placido.mattia@gmail.com>
2025-01-24 16:16:23 +0100

    Estetica

--- src/pages/AddPropertyPage.jsx
+++ src/pages/AddPropertyPage.jsx
@@ -6,7 +6,9 @@ export default function AddPropertyPage() {
   return (
     <Container className="mt-2 mb-5">
       <div className="text-center mb-5">
-        <h1>Aggiungi un nuovo immobile</h1>
+        <h1 className="fw-bold mb-4 homepage-most-searched text-center">
+          Aggiungi un nuovo immobile
+        </h1>
         <p className="text-secondary underline">
           Gli immobili saranno controllati da uno staff prima di essere aggiunti
         </p>
