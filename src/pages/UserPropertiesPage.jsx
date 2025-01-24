11e4d0f586ab59184bdfeceadbeeed67d7121f9b
MattiaPlacido <placido.mattia@gmail.com>
2025-01-24 16:13:03 +0100

    estetica

--- src/pages/UserPropertiesPage.jsx
+++ src/pages/UserPropertiesPage.jsx
@@ -7,7 +7,9 @@ export default function UserPropertiesPage() {
   return (
     <>
       <div className="container min-vh-100">
-        <h1 className="text-center fw-bold mb-3">I tuoi immobili</h1>
+        <h1 className="fw-bold mb-4 homepage-most-searched text-center">
+          I tuoi immobili
+        </h1>
         <div className="row row-cols-1 row-cols-xl-2 g-4 homepage-card-container">
           {userProperties.map((property, index) => {
             return <GeneralPropertyCard key={index} element={property} />;
