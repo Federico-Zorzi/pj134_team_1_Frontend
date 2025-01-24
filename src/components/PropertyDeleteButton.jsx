c605d8f9f07fb67d3a1a5de0705da3be4f20a518
MattiaPlacido <placido.mattia@gmail.com>
2025-01-24 12:14:12 +0100

    fix

--- src/components/PropertyDeleteButton.jsx
+++ src/components/PropertyDeleteButton.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useDataContext } from "../context/dataContext";
 import { Button, Modal } from "react-bootstrap";
