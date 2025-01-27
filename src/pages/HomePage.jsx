a2986734384ab96e9daff642c379c56bbc167f11
Alex-Alessi <alex.alessi246@gmail.com>
2025-01-27 11:47:15 +0100

    title fixed

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -44,10 +44,15 @@ export default function HomePage() {
   return (
     <main>
       <div className="container">
-        <h1 className=" fw-bold text-center mb-2">
-          Prenota case al mare, ville, chalet e molto altro per le tue vacanze
-          in tutta Italia
+        <h1 className="fw-bold text-center text-shadow mt-2">
+          Prenota{" "}
+          <i className="fa-solid fa-suitcase-rolling fa-bounce fa-2xs "></i>{" "}
+          case al mare, ville, chalet
         </h1>
+        <h2 className="text-center text-shadow my-3 ">
+          e molto altro per le tue vacanze in tutta Italia!
+        </h2>
+
         <HomepageCarousel />
 
         <h2 className="fw-bold mb-4 homepage-most-searched">
