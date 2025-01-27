4529d9acf7ce58398ce0302e19af048e8e3e9369
Federico-Zorzi <federico.zorzi05@gmail.com>
2025-01-27 13:05:34 +0100

    reviews loader

--- src/components/AdvancedSearchCard.jsx
+++ src/components/AdvancedSearchCard.jsx
@@ -8,12 +8,15 @@ export default function AdvancedSearchCard(params) {
   const { isLoading, setIsLoading } = useDataContext();
   const property = params.element;
   const [reviewNumber, setReviewNumber] = useState(0);
+  const [isLoadingReviewsNum, setIsLoadingReviewsNum] = useState(false);
 
   const fetchIndexReviews = () => {
+    setIsLoadingReviewsNum(true);
     fetch(`http://localhost:3000/properties/${property.id}/reviews`)
       .then((res) => res.json())
       .then((data) => {
         setReviewNumber(data.length);
+        setIsLoadingReviewsNum(false);
       });
   };
   useEffect(fetchIndexReviews, []);
@@ -109,7 +112,15 @@ export default function AdvancedSearchCard(params) {
                 <Row className="align-items-end">
                   <Col>
                     <span className="card-text col-6 fw-bold">
-                      {reviewNumber ? reviewNumber : "0"} recensioni
+                      {isLoadingReviewsNum ? (
+                        <span
+                          className="spinner-border spinner-border-reviews spinner-border-sm"
+                          aria-hidden="true"
+                        ></span>
+                      ) : (
+                        reviewNumber
+                      )}{" "}
+                      recensioni
                     </span>
                   </Col>
                   <Col className="text-end">
