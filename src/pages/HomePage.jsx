f347c483daf0615e4055db2d22d594f265c79843
MattiaPlacido <placido.mattia@gmail.com>
2025-01-28 12:38:53 +0100

    spacing homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -98,7 +98,7 @@ export default function HomePage() {
               ) : (
                 ""
               )}
-              <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 g-3 homepage-card-container">
+              <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 g-3 homepage-card-container pb-5">
                 {/* Cards */}
                 {restrictedMostPopPropertiesList.map((property) => {
                   return (
