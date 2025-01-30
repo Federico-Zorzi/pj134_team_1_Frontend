530b53d8fafd3b6249d929f004eb68bea952f5f6
FrancescoGiommi <francescogiommi97@icloud.com>
2025-01-30 12:44:24 +0100

    Add fix responsive ShowPage

--- src/pages/ShowPage.jsx
+++ src/pages/ShowPage.jsx
@@ -61,7 +61,7 @@ export default function ShowPage() {
             <div className="col-xl-6 col-md-12 mt-3">
               <div className="row d-flex">
                 {/* Indirizzo*/}
-                <div className="xs-device col-lg-12 col-md-4 col-sm-6  mt-2">
+                <div className="xs-device w-sm col-lg-12 col-md-4 col-sm-6  mt-2 w-xl w-lg w-md">
                   <i className="fa-solid fa-location-dot icon-style me-2"></i>
 
                   <strong>Indirizzo</strong>
@@ -69,56 +69,57 @@ export default function ShowPage() {
                 </div>
 
                 {/* Tipologia */}
-                <div className="xs-device col-lg-12 col-md-4 col-sm-6  mt-2">
+                <div className="xs-device w-sm col-lg-12 col-md-4 col-sm-6  mt-2 w-xl w-lg w-md">
                   <i className="fa-solid fa-building icon-style me-2"></i>
 
-                  <strong>Tipologia di immobile</strong>
+                  <strong>
+                    <span className="xs-detail">Tipologia di</span> Immobile
+                  </strong>
 
                   <p>{translatePropertyType(property.property_type)}</p>
                 </div>
                 {/* Numero Stanze */}
-                <div className="xs-device col-lg-12 col-md-4 col-sm-6 mt-2">
+                <div className="xs-device w-sm col-lg-12 col-md-4 col-sm-6 mt-2 w-xl w-lg w-md">
                   <i className="fa-solid fa-door-open icon-style me-2"></i>
 
-                  <strong>Numero di stanze</strong>
+                  <strong>
+                    <span className="xs-detail">Numero di</span> Stanze
+                  </strong>
                   <p>{property.number_of_rooms}</p>
                 </div>
                 {/* Numero Bagni */}
-                <div className="xs-device col-lg-12 col-md-4 col-sm-6 mt-2">
+                <div className="xs-device w-sm col-lg-12 col-md-4 col-sm-6 mt-2 w-xl w-lg w-md">
                   <i className="fa-solid fa-bath icon-style me-2"></i>
 
-                  <strong>Numero di bagni</strong>
+                  <strong>
+                    <span className="xs-detail">Numero di</span> Bagni
+                  </strong>
                   <p>{property.number_of_bathrooms}</p>
                 </div>
                 {/* Numero Letti */}
-                <div className="xs-device col-lg-12 col-md-4 col-sm-6 mt-2">
+                <div className="xs-device w-sm col-lg-12 col-md-4 col-sm-6 mt-2 w-xl w-lg w-md">
                   <i className="fa-solid fa-bed icon-style me-2"></i>
 
-                  <strong>Numero di letti</strong>
+                  <strong>
+                    <span className="xs-detail">Numero di</span> Letti
+                  </strong>
                   <p>{property.number_of_beds}</p>
                 </div>
                 {/* Metri Quadrati */}
-                <div className="xs-device col-lg-12 col-md-4 col-sm-6 mt-2">
+                <div className="xs-device w-sm col-lg-12 col-md-4 col-sm-6 mt-2 w-xl w-lg w-md">
                   <i className="fa-solid fa-expand icon-style me-2"></i>
                   <strong>Metri quadrati</strong>
                   <p>{property.square_meters}</p>
                 </div>
                 {/* Email */}
-                <div className="xs-device col-lg-12 col-md-4 col-sm-6 mt-2 mt-2">
+                <div className="xs-device w-sm col-lg-12 col-md-4 col-sm-6 mt-2 mt-2">
                   <i className="fa-solid fa-envelope icon-style me-2"></i>
                   <strong>Email di riferimento</strong>
                   <p>{property.reference_email}</p>
                 </div>
-
-                {/* Descrizione */}
-                <div className="mt-2">
-                  <i className="fa-solid fa-book icon-style me-2"></i>
-                  <strong>Descrizione</strong>
-                  <p>{property.description}</p>
-                </div>
               </div>
 
-              <div className="d-flex gap-2 mt-3">
+              <div className="d-flex gap-2 mt-5">
                 {/* Bottone per invio mail */}
                 <SendMailForm property={property} />
                 {/* Bottone per modificare l'immobile */}
@@ -126,6 +127,12 @@ export default function ShowPage() {
                 {/* Bottone per cancellare l'immobile */}
                 <PropertyDeleteButton propertyId={id} />
               </div>
+            </div>
+            {/* Descrizione */}
+            <div className="mt-2">
+              <i className="fa-solid fa-book icon-style me-2 mt-4"></i>
+              <strong>Descrizione</strong>
+              <p>{property.description}</p>
             </div>
           </div>
           <hr />
